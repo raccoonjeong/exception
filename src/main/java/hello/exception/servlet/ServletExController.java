3f1fb7a67e353f7c5180de4a7cfd757979b4c3a0
raccoonjeong <rana3782@gmail.com>
2023-05-23 21:02:07 +0900

    ..

---
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -0,0 +1,11 @@
+package hello.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ServletExController {
+
+}
